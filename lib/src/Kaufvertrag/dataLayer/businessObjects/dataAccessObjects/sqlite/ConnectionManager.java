352c99e4a65a2c2b80699b2f4200a25a15c774cf
jasha <meusel.jasha99@gmail.com>
2023-09-27 08:48:52 +0200

    bullshit

--- lib/src/Kaufvertrag/dataLayer/businessObjects/dataAccessObjects/sqlite/ConnectionManager.java
+++ lib/src/Kaufvertrag/dataLayer/businessObjects/dataAccessObjects/sqlite/ConnectionManager.java
@@ -1,25 +1,70 @@
 package Kaufvertrag.dataLayer.businessObjects.dataAccessObjects.sqlite;
 
+
+import Kaufvertrag.exceptions.DaoException;
+
 import java.sql.*;
 
 public class ConnectionManager {
 
-    private String CLASSNAME;
-    private String CONNECTIONSTRING;
-    private boolean classLoaded;
+    final private static String className = "org.sqlite.JDBC";
+    final private static String connectionString = "jdbc:sqlite:sqlite.db";
 
-    public Connection getNewConnection() {
-        return null; //PLACEHOLDER
+    private static Connection existingConnection;
+    private static boolean classLoaded;
+
+    public Connection getNewConnection() throws ClassNotFoundException {
+        // Datenbankklasse dynamisch erzeugen.
+        Class.forName (className);
+        // Verbindung initialisieren.
+        String datei = "sqlite.db";  //Dateiname inklusive Pfad.
+        String url = "jdbc:sqlite:" + datei;
+        // Verbindung aufbauen.
+        try {
+            existingConnection = DriverManager.getConnection(url);
+            String createAdresseTableSQL = "CREATE TABLE IF NOT EXISTS Adresse (" +
+                    "Id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "Strasse TEXT," +
+                    "Stadt TEXT," +
+                    "Plz TEXT" +
+                    ")";
+            existingConnection.createStatement().execute(createAdresseTableSQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+            try {
+            String createVertragspartnerTableSQL = "CREATE TABLE IF NOT EXISTS Vertragspartner (" +
+                    "Id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "Vorname TEXT," +
+                    "Nachname TEXT," +
+                    "Ausweisnr TEXT," +
+                    "Adresse_Id INTEGER," + // Fremdschlüssel auf die "adresse" Tabelle
+                    "FOREIGN KEY (Adresse_Id) REFERENCES Adresse(Id)" +
+                    ")";
+            existingConnection.createStatement().execute(createVertragspartnerTableSQL);
+            }  catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return  existingConnection;
     }
 
+    //TODO: prüfen ob bullshit?
     public Connection getExistingConnection() {
-        return null; //PLACEHOLDER
+        if(existingConnection != null) return existingConnection;
+        else {
+            try {
+                return getNewConnection();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void close(ResultSet resultSet, Statement statement, Connection connection){
-        resultSet = null; //PLACEHOLDER
-        statement = null; //PLACEHOLDER
-        connection = null; //PLACEHOLDER
+        resultSet = null;
+        statement = null;
+        connection = null;
     }
 
 
--- lib/src/Kaufvertrag/dataLayer/businessObjects/dataAccessObjects/sqlite/VertragspartnerDaoSqlite.java
+++ lib/src/Kaufvertrag/dataLayer/businessObjects/dataAccessObjects/sqlite/VertragspartnerDaoSqlite.java
@@ -1,39 +1,100 @@
 package Kaufvertrag.dataLayer.businessObjects.dataAccessObjects.sqlite;
 
+import Kaufvertrag.Main;
 import Kaufvertrag.businessObjects.IVertragspartner;
+import Kaufvertrag.dataLayer.businessObjects.Adresse;
+import Kaufvertrag.dataLayer.businessObjects.Vertragspartner;
 import Kaufvertrag.dataLayer.businessObjects.dataAccessObjects.IDao;
+import Kaufvertrag.dataLayer.businessObjects.dataAccessObjects.UIManager;
 import Kaufvertrag.exceptions.DaoException;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
-public class VertragspartnerDaoSqlite implements IDao<IVertragspartner, String> {
+public class VertragspartnerDaoSqlite implements IDao { //calls connectManager
+
+
+    public IVertragspartner creat() {
+        UIManager ui = UIManager.getInstance();
+
+        System.out.println("Wie lautet der Vorname des Vertragspartners?");
+        String vorname = Main.sc.next();
+        System.out.println("Wie lautet der Nachname des Vertragspartners?");
+        String nachname = Main.sc.next();
+        Vertragspartner vertragspartner = new Vertragspartner(vorname, nachname);
+
+        UIManager.AnswerOption<Object>  jaAt = ui.new AnswerOption<>(() -> {
+          vertragspartner.setAusweisNr("");
+          return null;
+        }, "Ja");
+        UIManager.AnswerOption<Object> neinAt = ui.new AnswerOption<>(null, "Nein");
+        UIManager.ConsoleOptions("Möchten Sie dem Vertragspartner eine Ausweisnummer zuweisen?", jaAt, neinAt);
+        jaAt = ui.new AnswerOption<>(() -> {
+            String strasse = ui.returnInput(
+                    "Geben Sie einen Straßennamen ein.",
+                    "^[-A-Za-z\\s]*$",
+                    "Kein gültiges format für einen Straßennamen."
+            );
+            String hausNr = ui.returnInput(
+                    "Geben Sie einen Hausnummer ein.",
+                    "\\b\\d+\\S*\\b",
+                    "Kein gültiges format für eine Hausnummer."
+            );
+            String plz = ui.returnInput(
+                    "Geben Sie einen Postleitzahl ein.",
+                    "\\b\\d{5}\\b",
+                    "Kein gültiges format für eine Postleitzahl."
+            );
+            String ort = ui.returnInput(
+                    "Geben Sie einen Ort ein.",
+                    "\\b\\w+\\b",
+                    "Kein gültiges format für einen Ort."
+            );
+            vertragspartner.setAdresse(new Adresse(strasse, hausNr, plz, ort));
+            return null;
+        },"Ja");
+        neinAt = ui.new AnswerOption<>(null, "Nein");
+        UIManager.ConsoleOptions("Möchten Sie dem Vertragspartner eine Adresse zuornen?");
+        return vertragspartner;
+
+        //Connection connection = new ConnectionManager().getNewConnection();
+        //String insertSQL = "INSERT INTO Vertragspartner (Vorname, Nachname, Ausweisnr, Adresse_ID) VALUES ()";
+        //return null;
+    }
+
+
     @Override
-    public IVertragspartner create() {
-        return null;
+    public Object create() {
+
+        return  null;
     }
 
     @Override
-    public void create(IVertragspartner objectToInsert) throws DaoException {
-
+    public void create(Object objectToInsert) throws DaoException {
+        //iware
     }
 
     @Override
-    public IVertragspartner read(String id) throws DaoException {
+    public Object read(Object id) throws DaoException {
         return null;
+        //select auf tabelle genau eins
     }
 
     @Override
     public List readAll() throws DaoException {
         return null;
+        //select auf tabelle ALLE
     }
 
     @Override
-    public void update(IVertragspartner objectToUpdate) throws DaoException {
+    public void update(Object objectToUpdate) throws DaoException {
 
     }
 
     @Override
-    public void delete(String id) throws DaoException {
+    public void delete(Object id) throws DaoException {
 
     }
 }
